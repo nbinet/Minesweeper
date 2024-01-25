228012fd5194f8e2e098fcd9ce9b4fe0b64dec66
nbinet <nathan.binet99@gmail.com>
2024-01-26 00:32:32 +0100

    change colors !

--- src/js/script.js
+++ src/js/script.js
@@ -7,11 +7,10 @@ let grid = document.querySelector(".game");
 let mines = InsertMines();
 
 const gridGenerator = () => {
-  for (let i = 1; i <= nbCases; i++) {
-    let block =
-      "<svg id=" + i + " class='block' viewBox='-25 -25 50 50'></svg>";
-    grid.insertAdjacentHTML("beforeend", block);
-  }
+    for (let i = 1; i <= nbCases; i++) {
+        let block = "<div id=" + i + " class='block'></div>";
+        grid.insertAdjacentHTML("beforeend", block);
+    }
 };
 
 gridGenerator();
@@ -19,50 +18,50 @@ gridGenerator();
 var blocks = document.querySelectorAll(".block");
 
 const restart = () => {
-  let blocksToRestart = document.querySelectorAll(".showed");
-  blocksToRestart.forEach((element) => {
-    element.innerHTML = "";
-    element.classList.remove("showed");
-  });
-  mines.forEach((element) => {
-    let mine = document.getElementById(element);
-    mine.innerHTML = "";
-  });
-  mines = InsertMines();
-  document.body.style.backgroundColor = "white";
+    let blocksToRestart = document.querySelectorAll(".showed");
+    blocksToRestart.forEach((element) => {
+        element.innerHTML = "";
+        element.classList.remove("showed");
+    });
+    mines.forEach((element) => {
+        let mine = document.getElementById(element);
+        mine.innerHTML = "";
+    });
+    mines = InsertMines();
+    document.body.style.backgroundColor = "white";
 };
 
 let restartButton = document.querySelector(".restart");
 restartButton.addEventListener("click", function () {
-  restart();
+    restart();
 });
 
 blocks.forEach((block) => {
-  block.addEventListener("click", function () {
-    if (mines.includes(block.id)) {
-      GameOver(mines);
-    } else if (!block.classList.contains("showed")) {
-      ShowCase(block.id, mines);
-    }
-  });
+    block.addEventListener("click", function () {
+        if (mines.includes(block.id)) {
+            GameOver(mines);
+        } else if (!block.classList.contains("showed")) {
+            ShowCase(block.id, mines);
+        }
+    });
 });
 
 function GameOver(mines) {
-  document.body.style.backgroundColor = "red";
-  mines.forEach((element) => {
-    let show = document.getElementById(element);
-    show.innerHTML = svgMine;
-  });
+    document.body.style.backgroundColor = "red";
+    mines.forEach((element) => {
+        let show = document.getElementById(element);
+        show.innerHTML = svgMine;
+    });
 }
 
 function InsertMines() {
-  let memo = [];
-  for (let i = 0; i < nbMines; i++) {
-    let n = Math.ceil(Math.random() * nbCases).toString();
-    while (memo.includes(n)) {
-      n = Math.ceil(Math.random() * nbCases).toString();
+    let memo = [];
+    for (let i = 0; i < nbMines; i++) {
+        let n = Math.ceil(Math.random() * nbCases).toString();
+        while (memo.includes(n)) {
+            n = Math.ceil(Math.random() * nbCases).toString();
+        }
+        memo.push(n);
     }
-    memo.push(n);
-  }
-  return memo;
+    return memo;
 }
--- src/js/showCase.js
+++ src/js/showCase.js
@@ -1,85 +1,77 @@
 import { nbCases, nbMines, colorsList, withGrid, sizeblock } from "./const";
 
 const numberShowed = (nb) => {
-  return `<text font-size='${sizeblock}' x='-25%' y='20' fill='${
-    colorsList[nb - 1]
-  }'>${nb}</text>`;
+    // return `<text font-size='${sizeblock}' x='-25%' y='20' fill='${colorsList[nb - 1]}'>${nb}</text>`;
+    return `<div class="number" style="color:${colorsList[nb - 1]};">${nb}</div>`;
 };
 
 function CasesBeside(n) {
-  let cases = [n - withGrid, n + withGrid];
-  if (n % withGrid == 1) {
-    cases.push(n - withGrid + 1, n + 1, n + withGrid + 1);
-  } else if (n % withGrid == 0) {
-    cases.push(n - withGrid - 1, n - 1, n + withGrid - 1);
-  } else {
-    cases.push(
-      n - withGrid + 1,
-      n + 1,
-      n + withGrid + 1,
-      n - withGrid - 1,
-      n - 1,
-      n + withGrid - 1
-    );
-  }
-  return cases;
+    let cases = [n - withGrid, n + withGrid];
+    if (n % withGrid == 1) {
+        cases.push(n - withGrid + 1, n + 1, n + withGrid + 1);
+    } else if (n % withGrid == 0) {
+        cases.push(n - withGrid - 1, n - 1, n + withGrid - 1);
+    } else {
+        cases.push(n - withGrid + 1, n + 1, n + withGrid + 1, n - withGrid - 1, n - 1, n + withGrid - 1);
+    }
+    return cases;
 }
 
 function isMinesbeside(button, mines) {
-  let memo = 0;
-  CasesBeside(Number(button)).forEach((element) => {
-    if (mines.includes(element.toString())) {
-      memo++;
-    }
-  });
-  return memo;
+    let memo = 0;
+    CasesBeside(Number(button)).forEach((element) => {
+        if (mines.includes(element.toString())) {
+            memo++;
+        }
+    });
+    return memo;
 }
 
 function Win() {
-  document.body.style.backgroundColor = "green";
+    document.body.style.backgroundColor = "green";
 }
 
 export function ShowCase(button, mines) {
-  let show = document.getElementById(button);
-  if (show.classList.contains("showed")) {
-  } else {
-    show.classList.add("showed");
-    if (document.querySelectorAll(".showed").length == nbCases - nbMines) {
-      Win();
+    let show = document.getElementById(button);
+    if (show.classList.contains("showed")) {
+    } else {
+        show.classList.add("showed");
+        if (document.querySelectorAll(".showed").length == nbCases - nbMines) {
+            Win();
+        }
+        switch (isMinesbeside(button, mines)) {
+            case 0:
+                show.innerHTML = "";
+                CasesBeside(Number(button)).forEach((element) => {
+                    if (element > 0 && element <= nbCases) {
+                        ShowCase(element, mines);
+                    }
+                });
+                break;
+            case 1:
+                show.innerHTML = numberShowed(1);
+                break;
+            case 2:
+                show.innerHTML = numberShowed(2);
+                break;
+            case 3:
+                show.innerHTML = numberShowed(3);
+                break;
+            case 4:
+                show.innerHTML = numberShowed(4);
+                break;
+            case 5:
+                show.innerHTML = numberShowed(5);
+                break;
+            case 6:
+                show.innerHTML = numberShowed(6);
+                break;
+            case 7:
+                show.innerHTML = numberShowed(7);
+                break;
+            case 8:
+                show.innerHTML = numberShowed(8);
+                break;
+        }
     }
-    switch (isMinesbeside(button, mines)) {
-      case 0:
-        show.innerHTML = "";
-        CasesBeside(Number(button)).forEach((element) => {
-          if (element > 0 && element <= nbCases) {
-            ShowCase(element, mines);
-          }
-        });
-        break;
-      case 1:
-        show.innerHTML = numberShowed(1);
-        break;
-      case 2:
-        show.innerHTML = numberShowed(2);
-        break;
-      case 3:
-        show.innerHTML = numberShowed(3);
-        break;
-      case 4:
-        show.innerHTML = numberShowed(4);
-        break;
-      case 5:
-        show.innerHTML = numberShowed(5);
-        break;
-      case 6:
-        show.innerHTML = numberShowed(6);
-        break;
-      case 7:
-        show.innerHTML = numberShowed(7);
-        break;
-      case 8:
-        show.innerHTML = numberShowed(8);
-        break;
-    }
-  }
 }
